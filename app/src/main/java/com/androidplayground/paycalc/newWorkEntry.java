6b63351f2be55585026fa1816b836680126b2498
Perpetua-Kabute <kabuteperpetua@gmail.com>
2020-05-31 11:22:44 +0300

    Feat:Changed layout to master/detail layout

--- app/src/main/java/com/androidplayground/paycalc/newWorkEntry.java
+++ app/src/main/java/com/androidplayground/paycalc/newWorkEntry.java
@@ -3,11 +3,11 @@ package com.androidplayground.paycalc;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class worker extends AppCompatActivity {
+public class newWorkEntry extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_worker);
+        setContentView(R.layout.activity_new_work_entry);
     }
 }
