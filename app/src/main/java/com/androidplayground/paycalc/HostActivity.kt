f85b3ef865a0915e031b3ba938ba1732b4802fb8
Perpetua-Kabute <kabuteperpetua@gmail.com>
2020-11-06 11:21:48 +0300

    feat: Create host activity for fragments

---
+++ app/src/main/java/com/androidplayground/paycalc/HostActivity.kt
@@ -0,0 +1,10 @@
+package com.androidplayground.paycalc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
