8505b43a5b35fb3567b21fbb85632f37f4ea367f
Perpetua-Kabute <kabuteperpetua@gmail.com>
2020-11-06 12:39:48 +0300

    feat: setContentView

--- app/src/main/java/com/androidplayground/paycalc/HostActivity.kt
+++ app/src/main/java/com/androidplayground/paycalc/HostActivity.kt
@@ -2,9 +2,12 @@ package com.androidplayground.paycalc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class HostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_host)
+        Log.i("HostActivity", "Activity created")
     }
 }
