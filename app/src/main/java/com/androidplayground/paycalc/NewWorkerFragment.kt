fad934ec227e510bacf6264fb83a0fedceaea6b5
Perpetua-Kabute <kabuteperpetua@gmail.com>
2020-11-06 12:40:48 +0300

    feat: log fragment created

--- app/src/main/java/com/androidplayground/paycalc/NewWorkerFragment.kt
+++ app/src/main/java/com/androidplayground/paycalc/NewWorkerFragment.kt
@@ -1,15 +1,12 @@
 package com.androidplayground.paycalc
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -27,6 +24,7 @@ class NewWorkerFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
+        Log.i("NewWorkerFragment","Fragment created")
         return inflater.inflate(R.layout.fragment_new_worker, container, false)
     }
 
